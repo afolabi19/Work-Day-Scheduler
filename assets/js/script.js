03b3f815a1d17d4d60b6232a49cfb9d7ad9c469b
Afolabi <mrfolabi@gmail.com>
2021-12-05 19:27:25 -0600

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,20 @@ var now = moment().format('H')
 
 // Create Object for tasks
 
-var tasks = [];
+var task = [];
 
-$(".saveBtn").on("click",function(){
+$(".time-block").on("click", "button", function(){
 
 console.log(this)
 
 
-var descriptionInput = document.querySelector("input[name='task-name']").value;
-var descriptionID = document.querySelector("input[name='task-name']").id
+var descriptionInput = $(this).parent().find("input").val();
+var descriptionID = $(this).parent().find(".col-10").attr("id");
 
-tasks.push({
+console.log(descriptionInput)
+ console.log(descriptionID)
+
+task.push({
 
     todo: descriptionInput,
     id: descriptionID
@@ -39,7 +42,7 @@ saveTasks()
 
 
 var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(task));
   };
   
   var loadTasks = function() {
@@ -59,68 +62,3 @@ var saveTasks = function() {
 }
 
 loadTasks()
-
-// var taskArr = [];
-
-// var today= moment().format('dddd, MMMM Do');
-// $("#currentDay").text(today);
-
-// function auditTask(taskEl) {
-//     var hour = $(taskEl).attr("id");
-//     hour = parseInt(hour);
-//     var time = moment().set({'hour': hour, minute: 00});
-//     var pastTime = moment().set({'hour': hour+1, minute: 00});
-//     if (moment().isAfter(pastTime)) {
-//         taskEl.removeClass("bg-success bg-danger");
-//         taskEl.addClass("alert-dark");
-//     } else if (moment().isAfter(time)) {
-//         taskEl.removeClass("bg-success alert-dark");
-//         taskEl.addClass("bg-danger");
-//     } else {
-//         taskEl.removeClass("bg-danger alert-dark");
-//         taskEl.addClass("bg-success");
-//     }
-// }
-
-// function checkTasks() {
-//     for (i=9; i<=17; i++) {
-//         var currentTaskString = '#'+i;
-//         var currentTask = $(currentTaskString)
-//         auditTask(currentTask);
-//     }
-// }
-
-// $(".hour").on("click","button", function() {
-//     console.log(this);
-//     var task = $(this).parent().find("textarea").val();
-//     var time = $(this).parent().find(".col-10").attr("id");
-//     taskArr.push({
-//         task: task,
-//         time: time
-//     });
-//     saveTasks();
-// });
-
-// function saveTasks() {
-//     localStorage.setItem("tasks", JSON.stringify(taskArr));
-// }
-
-// function loadTasks() {
-//     taskArr = JSON.parse(localStorage.getItem("tasks"));
-//     if(!taskArr){
-//         taskArr=[];
-//     }
-//         for(i=0; i<taskArr.length; i++) {
-//             var time = "#" + taskArr[i].time;
-//             var task = taskArr[i].task;
-//             console.log(time,task);
-//             $(time).val(task);
-//         }
-// }
-
-// setInterval(function(){
-//     checkTasks();
-// }, 60000);
-
-// loadTasks();
-// checkTasks(); //
